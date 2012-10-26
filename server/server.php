b1ea0058556510ee43e58fe812dab5469d35db72
alepapadop <alepapadop@gmail.com>
2012-10-26 16:16:02 +0300

    add function to remove  http chunk data size informatio

--- server/server.php
+++ server/server.php
@@ -99,67 +99,84 @@
         
         function socket_read_data($master_socket, $client)
         {
+            # Adding master_socket and the client into array to pass them to secket_select
             $this->client[0]=$client;
             $this->client[1]=$master_socket;
+            # Tracks if reading loop is over
             $flag=true;
+            # Stores the whole data received from th cpe
             $data=null;
+            # Tracks if a socket_close is called
+            $connection_state=true;
+            # Tracks is a session is established
             $session_established=false;
+            # Tracks the response from the cpe per read loop
             $temp=null;
+            # Makes a copy of the acs_functios in order to use them for a new connection
             $this->acs_functions=$this->acs_functions_init;
+            # Counts the commands that are called per session in order to call them one 
+            # by one. So each command will need a readind loop to be executed.
             $counter=0;
             
             # Set 5 sec timeout to client socket           
             socket_set_option($client,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>5, "usec"=>0));
             
             # Set up a blocking call to socket_select(), waits for a change in socket stastus
-            socket_select($this->client,$write = NULL, $except = NULL, $tv_sec= NULL);                        
+            socket_select($this->client,$write = NULL, $except = NULL, $tv_sec= NULL);                  
             
-            #reading loop for a specific session
+            # Reading loop for a specific session
             do{                                
                 
-                #reading data from the client
-                $input = socket_read($client,2048);                             
+                # Reading data from the client
+                $input = socket_read($client,2048);                                                                
                 
-                #error output
-                if($input===false)
-                    $this->mlog('Read error '.socket_strerror(socket_last_error())).chr(10);                                
+                # Error output
+                if($input===false && $connection_state==true)
+                    $this->mlog('Read error '.socket_strerror(socket_last_error())).chr(10);
                 
-                #the socket has timed out and the connection is closed
+                # The socket has timed out and the connection is closed
                 if($input==null)
                 {
+                    
+                    $datastring=$this->server_http_dechunk($data);
                     
                     print_r($data);
                     
                     socket_close($client);
                     
-                    unset($client);                   
+                    unset($client);
                     
                     unset($data);
                     
-                    unset($this->client);                                        
+                    unset($this->client);
                     
                     $flag= false;
                     
                     $this->mlog('Client ip: '.$this->peer_ip.' disconnected');
+                    
+                    
+                    
+                    print_r($datastring);
 
                 }
                 
-                #if there are any data from the socket
+                # If there are any data from the socket
                 if($input)
-                {   
+                {                                        
+                    
                     $data.=$input;
                                                             
                     $temp.=$input;
                     
-                    #if a session has been estrablished this part of code is executed.
+                    # If a session has been estrablished this part of code is executed.
                     if($session_established==true)
                     {
                         $counter++;
-                        $this->server_session($client,$counter);
+                        $this->server_session($client,$counter,$connection_state);
                     }
 
-                    #this means the modem has sent the complete response after the request from the acs.
-                    #The acs send an emptyResponse message in order to start a session with the cpe.
+                    # This means the modem has sent the complete response after the request from the acs.
+                    # The acs send an emptyResponse message in order to start a session with the cpe.
                     if(stripos($temp,'</soapenv:Envelope>')!==false && $session_established===false)
                     {   
                         
@@ -168,7 +185,7 @@
                         
                         $session_established=true;
                         
-                        $temp=null;                                                           
+                        $temp=null;                            
                     }                                        
                     
                 }
@@ -176,8 +193,8 @@
             }while($flag==true);
                                
         }
-        
-        private function server_session($client,& $counter)
+
+        private function server_session($client,& $counter,& $connection_state)
         {            
             
             $function=$this->acs_functions[0];
@@ -190,7 +207,9 @@
                             $this->server_session_getRPCMethods($client);
                             break;
                         case 2:
+                            socket_close($client);
                             unset($this->acs_functions[0]);
+                            $connection_state=false;
                             $counter=0;
                         break;
                     endswitch;
@@ -211,6 +230,7 @@
                             $this->mlog('Calling socket_close() Function');
                             socket_close($client);
                             unset($this->acs_functions[0]);
+                            $connection_state=false;
                             $counter=0;
                             break;
                     endswitch;
@@ -230,6 +250,7 @@
                             $this->mlog('Calling socket_close() Function');
                             socket_close($client);
                             unset($this->acs_functions[0]);
+                            $connection_state=false;
                             $counter=0;
                             break;
                     endswitch;
@@ -238,7 +259,7 @@
             endswitch;
             
         }
-                   
+   
         private function server_session_informResponse($client)
         {                        
             
@@ -251,7 +272,7 @@
             
             sleep(1);
         }
-        
+
         private function server_session_getRPCMethods($client)
         {
             $soap_obj=$this->write_obj['GetRPCMethods'];
@@ -263,7 +284,7 @@
             
             sleep(1);
         }
-        
+
         private function server_session_reboot($client)
         {
             $soap_obj=$this->write_obj['Reboot'];
@@ -275,7 +296,7 @@
             
             sleep(1);
         }
-        
+
         private function server_session_setParameter($client)
         {
             $soap_obj=$this->write_obj['SetParameterValues'];                        
@@ -287,7 +308,7 @@
             
             sleep(1);
         }
-        
+
         private function server_session_emptyResponse($client)
         {
             $soap_obj=$this->write_obj['EmptyResponse'];                        
@@ -299,6 +320,44 @@
             
             sleep(1);
         }
+        
+        # Function for removing chunk data size information
+        private function server_http_dechunk($data)
+        {
+            $data_array = explode("\r\n", $data);
+            
+            foreach($data_array as $line_num=>$line_string)
+            {
+                $pattern='/^([0-9A-E]){1,}/i';
+                $r=preg_match($pattern, trim($line_string),$match);
+                
+                # A line with a hex number number has been found
+                if($r)
+                    unset($data_array[$line_num]);
+                    
+            }
+            
+            return $data_array;
+            
+        }
+        
+        private function server_http_separate_header_soap($data)
+        {
+            
+        }
+        
+        private function is_hex($hex) 
+        {     
+            $hex = strtolower(trim(ltrim($hex,"0")));
+            if (empty($hex)) 
+            { 
+                $hex = 0; 
+                
+            }
+            $dec = hexdec($hex);
+            
+            return ($hex == dechex($dec));
+        } 
         
     }
     
